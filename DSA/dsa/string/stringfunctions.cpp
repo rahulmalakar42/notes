1198c3aec7f9b6ce14c51c14563298acbd249943
Rahul <malakarahul111@gmail.com>
2023-02-02 16:33:15 +0530

    commit

--- DSA/dsa/string/stringfunctions.cpp
+++ DSA/dsa/string/stringfunctions.cpp
@@ -3,7 +3,7 @@
 #include<string>
 using namespace std;    
 
-int main ()
+int main()
 {
   // string str ("look for non-alphabetic charactesrl"); //s1
 
@@ -32,22 +32,22 @@ int main ()
 
   // news.assign(olds,2,3) is same as news = olds.substr(2,3);
 
-  std::string str="to be question";
-  std::string str2="the ";
-  std::string str3="or not to be";
-  std::string::iterator it;
+  // std::string str="to be question";
+  // std::string str2="the ";
+  // std::string str3="or not to be";
+  // std::string::iterator it;
 
   // used in the same order as described above:
   // str.insert(6,str2);                 // to be (the )question
-  str.insert(6,str3,3,4);             // to be (not )the question
+  // str.insert(6,str3,3,4);             // to be (not )the question
   // str.insert(10,"that is cool",8);    // to be not (that is )the question
   // str.insert(10,"to be ");            // to be not (to be )that is the question
   // str.insert(15,1,':');               // to be not to be(:) that is the question
   // it = str.insert(str.begin()+5,','); // to be(,) not to be: that is the question
   // str.insert (str.end(),3,'.');       // to be, not to be: that is the question(...)
   // str.insert (it+2,str3.begin(),str3.begin()+3); // (or )
+  
 
-  std::cout << str << '\n';
   
   return 0;
 }
