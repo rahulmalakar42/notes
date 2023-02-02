9934a4d9b247aea9c07e2dd42dbfec08d3e93f70
Rahul <malakarahul111@gmail.com>
2023-02-02 19:09:32 +0530

    commit

--- DSA/dsa/recursion/recursion.cpp
+++ DSA/dsa/recursion/recursion.cpp
@@ -1,9 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void print(int i,int n){
+    if(i>n){
+        return;
+    }
+    cout<<n<<endl;
+   
+    print(i,n-1);
+}
+
 int main(){
-
-
+    int n; cin>>n;
+    print(1,n);
 
     return 0;
 }
